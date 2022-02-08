fe2c22a3a8844df6cfbc07144b84ac2680d81eb4
unknown <fadwaelalaoui0@gmail.com>
2022-02-08 10:39:12 +0100

    fcommit

--- src/UI_Interface/Affichage_voiture.java
+++ src/UI_Interface/Affichage_voiture.java
@@ -1,6 +1,7 @@
 package UI_Interface;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Spliterators;
@@ -14,6 +15,8 @@ public class Affichage_voiture extends JFrame {
 
 	public Affichage_voiture(Agence A) {
 		// TODO Auto-generated constructor stub
+		setSize(600,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		Iterator<Voiture> it;
 		it= A.voituresLouees();
@@ -34,13 +37,29 @@ public class Affichage_voiture extends JFrame {
 		}
 		
 		String[] columnNames = { "Marque", "Model", "Prix", "Anne Production" };
-		JTable table = new JTable(data,columnNames);
-		add (table, BorderLayout.CENTER);
 		
+		this.label();
+		this.table(data, columnNames);
 		
+		setVisible(true);
 		
 		
 	}
-	
+	public void  label()
+	{
+		JLabel labelHead = new JLabel("Liste des Voitures");
+        labelHead.setFont(new Font("Arial",Font.TRUETYPE_FONT,20));
+        this.getContentPane().add(labelHead,BorderLayout.PAGE_START);
+		
+	}
+	public void table(String [][] data,String[] columnNames ) {
+		
+		JTable table = new JTable(data,columnNames);
+		add (table, BorderLayout.CENTER);
+		JScrollPane scroll = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+		this.getContentPane().add(scroll,BorderLayout.CENTER);
+		
+	}
 	
 }
--- src/UI_Interface/First_Interface.java
+++ src/UI_Interface/First_Interface.java
@@ -1,22 +1,25 @@
 package UI_Interface;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-import metier.Agence;
+
+import metier.*;
 
 public class First_Interface extends JFrame {
 
 	JFrame fj = this;
+	JPanel panel= new JPanel();
 	public First_Interface(){
 		super("Agence De Location De Voiture ");
 		setSize(600,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
-		JPanel panel= new JPanel();
+		
 		add(panel, BorderLayout.SOUTH);
 		JButton B1 = new JButton("Afficher voitures");
 		JButton B2 = new JButton ("Chercher  par critères");
@@ -29,7 +32,7 @@ public class First_Interface extends JFrame {
 		panel.add(B4);
 		
 		panel.setBackground(Color.GRAY);
-		setVisible(true);
+		
 		
 		
 		B1.addActionListener(new ActionListener() {
@@ -37,11 +40,47 @@ public class First_Interface extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
+
+		      /*  JLabel labelHead = new JLabel("Liste des employes");
+		        labelHead.setFont(new Font("Arial",Font.TRUETYPE_FONT,20));
+		               fj.add(labelHead,BorderLayout.PAGE_START);*/
+		      
 				Agence A= new Agence();
+				Voiture V1 = new Voiture("Mercedece", "benz", 500,2020);
+				Voiture V2 = new Voiture("Audi", "benz", 500,2020);
+				Client  c1Client= new Client("Fadwa", "El ALAOUI", "BB183232", "CEL");
+				
+				A.add(V1);
+				A.add(V2);
+				A.louerVoiture(c1Client, V2);
+				A.louerVoiture(c1Client, V2);
+				
 			 Affichage_voiture AV =  new Affichage_voiture(A);
-			 fj.add(AV, BorderLayout.CENTER);
+			/*fj.add(AV, BorderLayout.CENTER);*/
 			}
 		});
+		B2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				  JButton inputButton = new JButton("Send");
+				    JTextArea editTextArea = new JTextArea("Type Here!");
+				    panel.add(inputButton);
+					panel.add(editTextArea);
+					setVisible(true);
+				    
+			}
+		});
+		 // this.add(labelHead,BorderLayout.PAGE_START);
+		
+		///JScrollPane scroll = new JScrollPane(table);
+        //table.setFillsViewportHeight(true);
+ 
+        //ajouter la table au frame
+        //frame.getContentPane().add(scroll,BorderLayout.CENTER);
+        
+		setVisible(true);
 	}
 	
 	
