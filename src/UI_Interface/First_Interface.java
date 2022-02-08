3164249a862fc7ff73e1aa0f0611a3fa3cc9f1c7
unknown <fadwaelalaoui0@gmail.com>
2022-02-08 10:45:14 +0100

    thiscommit1

--- src/UI_Interface/First_Interface.java
+++ src/UI_Interface/First_Interface.java
@@ -21,12 +21,12 @@ public class First_Interface extends JFrame {
 		setLayout(new BorderLayout());
 		
 		add(panel, BorderLayout.SOUTH);
-		JButton B1 = new JButton("Afficher voitures");
+		JButton B = new JButton("Afficher voitures");
 		JButton B2 = new JButton ("Chercher  par critères");
 		JButton B3 = new JButton("Louer Voiture");
 		JButton B4 = new JButton("Rendre Voiture");
 
-		panel.add(B1);
+		panel.add(B);
 		panel.add(B2);
 		panel.add(B3);
 		panel.add(B4);
@@ -35,15 +35,12 @@ public class First_Interface extends JFrame {
 		
 		
 		
-		B1.addActionListener(new ActionListener() {
+		B.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 
-		      /*  JLabel labelHead = new JLabel("Liste des employes");
-		        labelHead.setFont(new Font("Arial",Font.TRUETYPE_FONT,20));
-		               fj.add(labelHead,BorderLayout.PAGE_START);*/
 		      
 				Agence A= new Agence();
 				Voiture V1 = new Voiture("Mercedece", "benz", 500,2020);
@@ -56,7 +53,7 @@ public class First_Interface extends JFrame {
 				A.louerVoiture(c1Client, V2);
 				
 			 Affichage_voiture AV =  new Affichage_voiture(A);
-			/*fj.add(AV, BorderLayout.CENTER);*/
+			
 			}
 		});
 		B2.addActionListener(new ActionListener() {
@@ -72,14 +69,7 @@ public class First_Interface extends JFrame {
 				    
 			}
 		});
-		 // this.add(labelHead,BorderLayout.PAGE_START);
 		
-		///JScrollPane scroll = new JScrollPane(table);
-        //table.setFillsViewportHeight(true);
- 
-        //ajouter la table au frame
-        //frame.getContentPane().add(scroll,BorderLayout.CENTER);
-        
 		setVisible(true);
 	}
 	
